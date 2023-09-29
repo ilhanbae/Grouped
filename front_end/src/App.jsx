f3e1b787d47fbe4d4fdd8f175bd29ace23d0340b
Neil Bae <nbae0127@gmail.com>
2023-09-29 00:38:58 -0400

    Install React Tailwind Axios Heroicons

---
+++ front_end/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { BeakerIcon } from "@heroicons/react/24/solid";
+
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">This is our landing page</h1>
+      <BeakerIcon className="h-6 w-6 text-blue-500" />
+    </div>
+  );
+}
