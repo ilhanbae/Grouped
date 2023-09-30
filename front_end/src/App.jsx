03e912198460953829f8bcf910a0fab42cc27c63
lagao <lagao@buffalo.edu>
2023-09-30 19:29:28 -0400

    Create layout and input fields for SignUp page

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import { Profile } from './pages/Profile/Profile';
 import { EditProfile } from "./pages/EditProfile/EditProfile";
 import Login from "./pages/Login/Login";
+import SignUp from "./pages/SignUp/SignUp";
 import './index.css';
 
 export default function App() {
@@ -10,6 +11,7 @@ export default function App() {
         <div>
             <Routes>
                 <Route path='/login' element={<Login/>}/>
+                <Route path='/signup' element={<SignUp/>}/>
                 <Route path="/profile" element={<Profile />} />
                 <Route path="/editprofile" element={<EditProfile />} />
             </Routes>
