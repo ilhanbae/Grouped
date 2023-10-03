b9a50ab4341e99c37d11a6261c8d23b2d3952712
Neil Bae <nbae0127@gmail.com>
2023-10-03 17:27:52 -0400

    Add toggle dropdown on outside click hook

---
+++ front_end/src/components/Header/DropdownLinks.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const DropDownLinks = (props) => {
+  const links = [
+    { label: "Profile", url: "/profile" },
+    { label: "Manage Account", url: "/manageaccount" },
+  ];
+
+  return (
+    <ul className="absolute right-1 rounded-md bg-slate-200 p-1">
+      {links.map((link) => (
+        <Link to={link.url} onClick={props.toggleMenu}>
+          <li className="flex bg-slate-300 rounded-md p-2 m-1 hover:bg-slate-400">
+            {link.label}
+          </li>
+        </Link>
+      ))}
+    </ul>
+  );
+};
+
+export default DropDownLinks;
--- front_end/src/components/Header/ProfileDropdownMenu.jsx
+++ front_end/src/components/Header/ProfileDropdownMenu.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useRef, useEffect } from "react";
+import ProfileIcon from "./ProfileIcon";
+import DropdownLinks from "./DropdownLinks";
 
 const ProfileDropdownMenu = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const dropDownMenu = useRef(null);
 
   const user = {
     firstname: "John",
@@ -12,43 +14,32 @@ const ProfileDropdownMenu = () => {
     school: "University at Buffalo",
   };
   const userInitial = user.firstname[0] + user.lastname[0];
+
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  /* This hook checks if mousedown DOM event occurs outside the dropdown menu. */
+  useEffect(() => {
+    const outSideClickHandler = (event) => {
+      if (
+        dropDownMenu.current &&
+        !dropDownMenu.current.contains(event.target)
+      ) {
+        setIsMenuOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", outSideClickHandler);
+    return () => {
+      document.removeEventListener("mousedown", outSideClickHandler);
+    };
+  });
+
   return (
-    <div>
+    <div ref={dropDownMenu}>
       <ProfileIcon userInitial={userInitial} toggleMenu={toggleMenu} />
-      {isMenuOpen && <DropdownLinks />}
+      {isMenuOpen && <DropdownLinks toggleMenu={toggleMenu} />}
     </div>
-  );
-};
-
-const ProfileIcon = (props) => {
-  return (
-    <div
-      className="flex items-center justify-center w-10 h-10 rounded-full bg-slate-50 cursor-pointer"
-      onClick={props.toggleMenu}
-    >
-      <span className="font-bold">{props.userInitial}</span>
-    </div>
-  );
-};
-
-const DropdownLinks = (props) => {
-  const links = [
-    { label: "Profile", url: "/profile" },
-    { label: "Manage Account", url: "/manageaccount" },
-  ];
-
-  return (
-    <ul className="absolute right-1 rounded-md bg-slate-200 p-3 space-y-1">
-      {links.map((link) => (
-        <li className="bg-slate-300 rounded-md">
-          <Link to={link.url}>{link.label}</Link>
-        </li>
-      ))}
-    </ul>
   );
 };
 
