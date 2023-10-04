e1e63ca26e92f824347612f1c46e24770f217747
lagao <lagao@buffalo.edu>
2023-10-04 00:14:04 -0400

    Send POST request on Submit from form in Login Page

--- front_end/src/pages/Login/Login.jsx
+++ front_end/src/pages/Login/Login.jsx
@@ -1,8 +1,40 @@
+import React, { useState } from "react";
 import { Link } from 'react-router-dom';
 import { BiUser } from 'react-icons/bi';
 import { AiOutlineUnlock } from 'react-icons/ai';
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const data = {
+            email,
+            password,
+        };
+
+        try {
+            // Send POST request to server endpoint
+            const response = await fetch('/your-auth-endpoint', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            });
+
+            if (response.ok) {
+                console.log('Login successful');
+            } else {
+                console.error('Login failed');
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
+
+
     return (
         <div style={{ backgroundColor: 'lightgrey' }}>
             <div className="text-white flex flex-col justify-center items-center">
@@ -10,14 +42,14 @@ const Login = () => {
                 <br />
                 <div className="flex flex-col items-center">
                     <div className="bg-slate-800 border border-slate-800 rounded-md p-8 shadow-lg backdrop-filter backdrop-blur-sm bg-opacity-30 relative" style={{ backgroundColor: 'lightskyblue' }}>
-                        <form action="">
+                        <form onSubmit={handleSubmit}>
                             <div className="relative my-4">
-                                <input type="email" className="block w-96 py-4 px-4 text-lg text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-black focus:border-blue-600 peer" placeholder="" />
-                                <label htmlFor="" className="absolute text-lg text-black duration-300 transform -translate-y-6 scale-75 top-3 -x-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Your Email/Username</label>
+                                <input type="email" className="block w-96 py-4 px-4 text-lg text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-black focus:border-blue-600 peer" placeholder="" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                                <label htmlFor="" className="absolute text-lg text-black duration-300 transform -translate-y-6 scale-75 top-3 -x-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Your Email</label>
                                 <BiUser className="absolute top-4 right-4" />
                             </div>
                             <div className="relative my-4">
-                                <input type="password" className="block w-96 py-4 px-4 text-lg text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-black focus:border-blue-600 peer" placeholder="" />
+                                <input type="password" className="block w-96 py-4 px-4 text-lg text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-black focus:border-blue-600 peer" placeholder="" value={password} onChange={(e) => setPassword(e.target.value)}/>
                                 <label htmlFor="" className="absolute text-lg text-black duration-300 transform -translate-y-6 scale-75 top-3 -x-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Your Password</label>
                                 <AiOutlineUnlock className="absolute top-4 right-4" />
                             </div>
