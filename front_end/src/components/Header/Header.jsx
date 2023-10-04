39ad98d5713790e418f726b4ed1efe0606bd6d8a
Neil Bae <nbae0127@gmail.com>
2023-10-04 10:14:00 -0400

    Add header logo with navigation to individual calendar page

--- front_end/src/components/Header/Header.jsx
+++ front_end/src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ProfileDropdownMenu from "./ProfileDropdownMenu";
+import Logo from "./Logo";
 
 const Header = () => {
   return (
     <nav className="flex min-w-full items-center justify-between p-3 bg-slate-700">
-      <div>Logo</div>
+      <Logo />
       <ProfileDropdownMenu />
     </nav>
   );
