196f74b48acc618e2e4bf8382b2ad0116f33d4a0
lagao <lagao@buffalo.edu>
2023-10-09 19:36:12 -0400

    Create input fields from react-big-calendar

--- front_end/src/pages/IndividualCalendar/IndividualCalendar.jsx
+++ front_end/src/pages/IndividualCalendar/IndividualCalendar.jsx
@@ -1,7 +1,39 @@
-const IndividualCalendar = () => {
-    return (
-        <div>
+import {Calendar, momentLocalizer} from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import './IndividualCalendar.css';
+import 'moment-timezone';
 
+moment.tz.setDefault('America/New_York');
+const localizer = momentLocalizer(moment);
+
+const IndividualCalendar = (props) => {
+//     const state = {
+//         events: [
+//           {
+//             start: moment().toDate(),
+//             end: moment()
+//               .add(1, "hours")
+//               .toDate(),
+//             title: "Some title"
+//           }
+//         ]
+//     };
+
+    return (
+        <div classname="calendarApp" style={{backgroundColor:"lightgray"}}>
+            <br/>
+            <div>
+                <Calendar
+                    localizer={localizer}
+                    defaultDate={new Date()}
+                    defaultView="week"
+//                     events={state.events}
+                    startAccessor="start"
+                    endAccessor="end"
+                    style={{height: "70vh", padding:"10px 10px 10px 10px"}}
+                />
+            </div>
         </div>
     );
 };
