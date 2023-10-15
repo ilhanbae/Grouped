04fec20298f72cf1f46f91d478d885cd92d9fe52
Neil Bae <nbae0127@gmail.com>
2023-10-15 19:03:19 -0400

    Add login form vaildation using react-hook-form and yup

--- front_end/src/pages/Login/Login.jsx
+++ front_end/src/pages/Login/Login.jsx
@@ -1,72 +1,145 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from 'react-router-dom';
-import { BiUser } from 'react-icons/bi';
-import { AiOutlineUnlock } from 'react-icons/ai';
+import { Link, useNavigate } from "react-router-dom";
+import { BiUser } from "react-icons/bi";
+import { AiOutlineUnlock } from "react-icons/ai";
 import axios from "axios";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 const Login = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const data = {
-            email,
-            password,
-        };
+  // Yup Schema
+  const loginSchema = yup.object().shape({
+    email: yup
+      .string()
+      .email("Email should be a valid email address")
+      .required("Email is required."),
+    password: yup.string().required("Password is required."),
+  });
 
-        try {
-            // Send POST request to server endpoint
-            const response = await axios.post('/your-auth-endpoint', data, {
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            });
+  // useForm hook
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(loginSchema),
+    mode: "onSubmit",
+    reValidateMode: "onSubmit",
+    // reValidateMode: "onChange",
+  });
 
-            if (response === 200) {
-                console.log('Login successful');
-                navigate('/individualCalendar');
-            } else {
-                console.error('Login failed');
-            }
-        } catch (error) {
-            console.error('Error:', error);
-        }
-        //TODO: for now, to establish connections, this page will lead you to the calendar view page
-        window.location.href = "http://localhost:3000/individualCalendar";
-    };
+  // Send POST request to login a user
+  const loginUser = async (data) => {
+    await axios
+      .post(`${process.env.REACT_APP_API_URL}/login.php`, data)
+      .then((response) => {
+        console.log("Login successful!");
+        // Update session storage
+        sessionStorage.setItem("id", response.data.id); // user's id
+        sessionStorage.setItem("username", response.data.username); // user's username
+        sessionStorage.setItem("email", response.data.email); // user's email
+        navigate("/individualCalendar");
+      })
+      .catch((error) => {
+        console.error(error);
+        setError("email", {
+          type: "Email and password combination",
+          message: "Incorrect email address or password.",
+        });
+        setError("password", {
+          type: "Email and password combination",
+          message: "Incorrect email address or password.",
+        });
+      });
+  };
 
+  const onSubmit = async (data) => {
+    // console.log(data);
+    await loginUser(data);
+  };
 
-    return (
-        <div style={{ backgroundColor: 'lightgrey' }}>
-            <div className="text-white flex flex-col justify-center items-center">
-                <h1 className="text-6xl text-black font-bold text-center p-8">Login to Your Account</h1>
-                <br />
-                <div className="flex flex-col items-center">
-                    <div className="bg-slate-800 border border-slate-800 rounded-md p-8 shadow-lg backdrop-filter backdrop-blur-sm bg-opacity-30 relative" style={{ backgroundColor: 'lightskyblue' }}>
-                        <form onSubmit={handleSubmit}>
-                            <div className="relative my-4">
-                                <input type="email" className="block w-96 py-4 px-4 text-lg text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-black focus:border-blue-600 peer" placeholder="" value={email} onChange={(e) => setEmail(e.target.value)}/>
-                                <label htmlFor="" className="absolute text-lg text-black duration-300 transform -translate-y-6 scale-75 top-3 -x-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Your Email</label>
-                                <BiUser className="absolute top-4 right-4" />
-                            </div>
-                            <div className="relative my-4">
-                                <input type="password" className="block w-96 py-4 px-4 text-lg text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-black focus:border-blue-600 peer" placeholder="" value={password} onChange={(e) => setPassword(e.target.value)}/>
-                                <label htmlFor="" className="absolute text-lg text-black duration-300 transform -translate-y-6 scale-75 top-3 -x-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Your Password</label>
-                                <AiOutlineUnlock className="absolute top-4 right-4" />
-                            </div>
-                            <button className="w-full mb-4 text-[25px] mt-6 rounded-full bg-yellow-500 text-black font-bold hover:bg-yellow-600 hover:text-white py-2 transition-colors duration-300" type="submit">Sign In</button>
-                        </form>
-                    </div>
-                    <div className='p-8' style={{marginTop:'20px'}}>
-                        <span className='m-12'><Link className="text-blue-500 text-3xl font-bold" to="/signup">Don't have an Account?</Link></span>
-                        <span className='m-12'><Link className="text-blue-500 text-3xl font-bold" to="ForgotPassword">Forgot Password?</Link></span>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="bg-slate-300">
+      <div className="text-white flex flex-col justify-center items-center">
+        <h1 className="text-6xl text-black font-bold text-center p-8">
+          Login to Your Account
+        </h1>
+        <br />
+        <div className="flex flex-col items-center">
+          <div
+            className="bg-slate-800 border border-slate-800 rounded-md p-8 shadow-lg backdrop-filter backdrop-blur-sm bg-opacity-30 relative"
+            style={{ backgroundColor: "lightskyblue" }}
+          >
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              autoComplete="off"
+              noValidate
+            >
+              {/* Email */}
+              <div className="relative my-4">
+                <input
+                  type="email"
+                  className="block w-96 py-4 px-4 text-lg text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-black focus:border-blue-600 peer"
+                  placeholder=""
+                  {...register("email")}
+                />
+                <label
+                  htmlFor=""
+                  className="absolute text-lg text-black duration-300 transform -translate-y-6 scale-75 top-3 -x-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                >
+                  Your Email
+                </label>
+                <BiUser className="absolute top-4 right-4" />
+                <span className="block w-96">{errors.email?.message}</span>
+              </div>
+              {/* Password */}
+              <div className="relative my-4">
+                <input
+                  type="password"
+                  className="block w-96 py-4 px-4 text-lg text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-black focus:border-blue-600 peer"
+                  placeholder=""
+                  {...register("password")}
+                />
+                <label
+                  htmlFor=""
+                  className="absolute text-lg text-black duration-300 transform -translate-y-6 scale-75 top-3 -x-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                >
+                  Your Password
+                </label>
+                <AiOutlineUnlock className="absolute top-4 right-4" />
+                <span className="block w-96">{errors.password?.message}</span>
+              </div>
+              <button
+                className="w-full mb-4 text-[25px] mt-6 rounded-full bg-yellow-500 text-black font-bold hover:bg-yellow-600 hover:text-white py-2 transition-colors duration-300"
+                type="submit"
+              >
+                Sign In
+              </button>
+            </form>
+          </div>
+          <div className="p-8" style={{ marginTop: "20px" }}>
+            <span className="m-12">
+              <Link className="text-blue-500 text-3xl font-bold" to="/signup">
+                Don't have an Account?
+              </Link>
+            </span>
+            <span className="m-12">
+              <Link
+                className="text-blue-500 text-3xl font-bold"
+                to="ForgotPassword"
+              >
+                Forgot Password?
+              </Link>
+            </span>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Login;
