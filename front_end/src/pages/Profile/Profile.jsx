fa704160050f7a196e54447095a5d0ba4108efbd
Neil Bae <nbae0127@gmail.com>
2023-10-15 22:55:22 -0400

    Call user api when the profile page loads

--- front_end/src/pages/Profile/Profile.jsx
+++ front_end/src/pages/Profile/Profile.jsx
@@ -2,100 +2,117 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const Profile = () => {
+  const [isLoaded, setIsLoaded] = useState(false);
   const [userInfo, setUserInfo] = useState({
-    firstname: "John",
-    lastname: "Doe",
-    bio: "Hi!\nI am John Doe.",
-    email: "tester@example.com",
-    school: "University at Buffalo",
-    // firstname: "",
-    // lastname: "",
-    // bio: "",
-    // email: "",
-    // school: ""
+    firstname: "",
+    lastname: "",
+    bio: "",
+    email: "",
+    school: "",
   });
 
   useEffect(() => {
-    // loadUserInfo();
+    loadUserInfo();
   }, []);
 
-  const loadUserInfo = () => {
-    axios
-      .get(`${process.env.REACT_APP_API_URL}/endpoint`)
-      .then((response) => setUserInfo(response))
-      .catch((error) => console.log(error));
+  const loadUserInfo = async () => {
+    setIsLoaded(false);
+    await axios
+      .get(`${process.env.REACT_APP_API_URL}/user.php`, {
+        params: {
+          email: sessionStorage.getItem("email"),
+        },
+      })
+      .then((response) => {
+        console.log(response.data);
+        setUserInfo({
+          firstname: response.data.firstName ? response.data.firstName : "",
+          lastname: response.data.lastName ? response.data.lastName : "",
+          bio: response.data.bio ? response.data.bio : "",
+          email: response.data.email ? response.data.email : "",
+          school: response.data.school ? response.data.school : "",
+        });
+      })
+      .catch((error) => console.error(error));
+    setIsLoaded(true);
   };
 
-  return (
-    <div className="flex items-center flex-col bg-slate-300">
-      {/* User Fields */}
-      <div className="bg-slate-300 p-8 rounded-lg flex flex-col w-72">
-        {/* First Name */}
-        <div className="mb-4 flex flex-col">
-          <label className="text-gray-600" htmlFor="firstname">
-            First Name:
-          </label>
-          <input
-            className="rounded-md w-full px-1 bg-slate-100 text-gray-800"
-            id="firstname"
-            type="text"
-            value={userInfo.firstname}
-            readOnly
-          />
-        </div>
-        {/* Last Name */}
-        <div className="mb-4 flex flex-col">
-          <label className="text-gray-600" htmlFor="lastname">
-            Last Name:
-          </label>
-          <input
-            className="rounded-md w-full px-1 bg-slate-100 text-gray-800"
-            id="lastname"
-            type="text"
-            defaultValue={userInfo.lastname}
-            readOnly
-          />
-        </div>
-        {/* Bio */}
-        <div className="mb-4 flex flex-col">
-          <label className="text-gray-600" htmlFor="bio">
-            Bio:
-          </label>
-          <textarea
-            className="rounded-md px-1 bg-slate-100 text-gray-800 resize-none"
-            id="bio"
-            defaultValue={userInfo.bio}
-            rows={6}
-            readOnly
-          />
-        </div>
-        {/* Email */}
-        <div className="mb-4 flex flex-col">
-          <label className="text-gray-600" htmlFor="email">
-            Email:
-          </label>
-          <input
-            className="rounded-md w-full px-1 bg-slate-100 text-gray-800"
-            id="email"
-            type="text"
-            defaultValue={userInfo.email}
-            readOnly
-          />
-        </div>
-        {/* School */}
-        <div className="mb-4 flex flex-col" htmlFor="school">
-          <label className="text-gray-600">School:</label>
-          <input
-            className="rounded-md w-full px-1 bg-slate-100 text-gray-800"
-            id="school"
-            type="text"
-            defaultValue={userInfo.school}
-            readOnly
-          />
+  if (!isLoaded) {
+    return <div className="flex items-center justify-center">Loading...</div>;
+  } else {
+    return (
+      <div className="flex items-center flex-col bg-slate-300">
+        {/* User Fields */}
+        <div className="bg-slate-300 p-8 rounded-lg flex flex-col w-72">
+          {/* First Name */}
+          <div className="mb-4 flex flex-col">
+            <label className="text-gray-600" htmlFor="firstname">
+              First Name:
+            </label>
+            <input
+              className="rounded-md w-full px-1 bg-slate-100 text-gray-800"
+              id="firstname"
+              type="text"
+              value={userInfo.firstname}
+              readOnly
+            />
+          </div>
+          {/* Last Name */}
+          <div className="mb-4 flex flex-col">
+            <label className="text-gray-600" htmlFor="lastname">
+              Last Name:
+            </label>
+            <input
+              className="rounded-md w-full px-1 bg-slate-100 text-gray-800"
+              id="lastname"
+              type="text"
+              defaultValue={userInfo.lastname}
+              readOnly
+            />
+          </div>
+          {/* Bio */}
+          <div className="mb-4 flex flex-col">
+            <label className="text-gray-600" htmlFor="bio">
+              Bio:
+            </label>
+            <textarea
+              className="rounded-md px-1 bg-slate-100 text-gray-800 resize-none"
+              id="bio"
+              defaultValue={userInfo.bio}
+              rows={6}
+              readOnly
+            />
+          </div>
+          {/* Email */}
+          <div className="mb-4 flex flex-col">
+            <label className="text-gray-600" htmlFor="email">
+              Email:
+            </label>
+            <input
+              className="rounded-md w-full px-1 bg-slate-100 text-gray-800"
+              id="email"
+              type="text"
+              defaultValue={userInfo.email}
+              readOnly
+            />
+          </div>
+          {/* School */}
+          <div className="mb-4 flex flex-col" htmlFor="school">
+            <label className="text-gray-600">School:</label>
+            <input
+              className="rounded-md w-full px-1 bg-slate-100 text-gray-800"
+              id="school"
+              type="text"
+              defaultValue={userInfo.school}
+              readOnly
+            />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+
+
 };
 
 export default Profile;
