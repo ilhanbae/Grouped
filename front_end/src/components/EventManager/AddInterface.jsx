110c03229d8241d025f0d845da4796bbfdf71fcb
lagao <lagao@buffalo.edu>
2023-10-18 00:11:44 -0400

    Autoselection and Location field
    	- Add Event button autoselects Self in /individualCalendar
    	- Location field is created as text
    	- Instead of Red X to close, have a Delete button

--- front_end/src/components/EventManager/AddInterface.jsx
+++ front_end/src/components/EventManager/AddInterface.jsx
@@ -1,20 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import moment from 'moment';
 
-const AddInterface = ({ onClose, onSave, selectedEvent, fromCalendar }) => {
+const AddInterface = ({ onClose, onDelete, onSave, selectedEvent, fromCalendar }) => {
   const [title, setTitle] = useState('');
   const [start, setStart] = useState('');
   const [end, setEnd] = useState('');
+  const [location, setLocation] = useState('');
   const [description, setDescription] = useState('');
-  const [isSelfSelected, setIsSelfSelected] = useState(false);
-  const [isGroupSelected, setIsGroupSelected] = useState(false);
+  const [isSelfSelected, setIsSelfSelected] = useState(fromCalendar === 'individual');
+  const [isGroupSelected, setIsGroupSelected] = useState(!isSelfSelected);
 
   useEffect(() => {
     if (selectedEvent) {
       setTitle(selectedEvent.title);
       setDescription(selectedEvent.description);
 
-      // Format start and end dates to the correct string format
       const formattedStart = moment(selectedEvent.start).format('YYYY-MM-DDTHH:mm');
       const formattedEnd = moment(selectedEvent.end).format('YYYY-MM-DDTHH:mm');
 
@@ -33,6 +33,7 @@ const AddInterface = ({ onClose, onSave, selectedEvent, fromCalendar }) => {
       title,
       start,
       end,
+      location,
       description,
     };
 
@@ -52,13 +53,6 @@ const AddInterface = ({ onClose, onSave, selectedEvent, fromCalendar }) => {
 
   return (
     <div className="add-interface bg-[#e5e7eb] p-4 relative">
-      {/* Close Button */}
-      <button
-        className="close-button absolute top-2 right-2 bg-red-500 text-black w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
-        onClick={onClose}
-      >
-        <i className="text-xl">X</i>
-      </button>
       <div className="overlay-content bg-[#e5e7eb] flex flex-col">
         <input
           className="h-12 m-2 text-2xl bg-[#e5e7eb]"
@@ -105,6 +99,14 @@ const AddInterface = ({ onClose, onSave, selectedEvent, fromCalendar }) => {
           onChange={(e) => setEnd(e.target.value)}
         />
         <br />
+        <input
+          className="text-lg bg-[#e5e7eb]"
+          type="text"
+          placeholder="Location"
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
+        />
+        <br />
         <textarea
           id="description"
           rows="4"
@@ -113,7 +115,7 @@ const AddInterface = ({ onClose, onSave, selectedEvent, fromCalendar }) => {
           value={description}
           onChange={(e) => setDescription(e.target.value)}
         ></textarea>
-        <div style={{ textAlign: 'right' }}>
+        <div className="flex justify-end">
           <button
             onClick={handleSave}
             className="h-12 m-2 px-6 bg-gray-600 text-white"
@@ -126,6 +128,14 @@ const AddInterface = ({ onClose, onSave, selectedEvent, fromCalendar }) => {
           >
             Cancel
           </button>
+          {selectedEvent && (
+              <button
+                className="h-12 m-2 px-6 bg-red-500 text-black rounded-lg"
+                onClick={onDelete}
+              >
+                Delete
+              </button>
+              )}
         </div>
       </div>
     </div>
--- front_end/src/pages/IndividualCalendar/IndividualCalendar.jsx
+++ front_end/src/pages/IndividualCalendar/IndividualCalendar.jsx
@@ -1,4 +1,4 @@
-import {Calendar, momentLocalizer} from 'react-big-calendar';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import './IndividualCalendar.css';
@@ -10,63 +10,76 @@ moment.tz.setDefault('America/New_York');
 const localizer = momentLocalizer(moment);
 
 const IndividualCalendar = (props) => {
-    const state = {
-        events: [
-          {
-            start: moment().toDate(),
-            end: moment().add(1, "hours").toDate(),
-            title: "Some title",
-            description: 'Some description'
-          },
-          {
-            start: moment().toDate(),
-            end: moment().add(2, "hours").toDate(),
-            title: "Another title",
-            description:'Another description'
-          }
-        ]
-    };
+  const [events, setEvents] = useState([
+    {
+      start: moment().toDate(),
+      end: moment().add(1, 'hours').toDate(),
+      title: 'Some title',
+      description: 'Some description',
+      location: 'Some location',
+    },
+    {
+      start: moment().toDate(),
+      end: moment().add(2, 'hours').toDate(),
+      title: 'Another title',
+      description: 'Another description',
+      location: 'Another location',
+    },
+  ]);
 
-    const [selectedEvent, setSelectedEvent] = useState(null);
-    const handleEventSelect = (event) => {
-        event.start = moment(event.start);
-        event.end = moment(event.end);
-        setSelectedEvent(event);
-    };
-    const handleSave = (event) => {
-        console.log('Event saved:', event);
-    };
-    const handleClose = () => {
-        setSelectedEvent(null);
-    };
+  const [selectedEvent, setSelectedEvent] = useState(null);
 
-    return (
-        <div className="calendarApp" style={{backgroundColor:"lightgray"}}>
-            <br/>
-            <div>
-                <Calendar
-                    localizer={localizer}
-                    defaultDate={new Date()}
-                    defaultView="week"
-                    events={state.events}
-                    startAccessor="start"
-                    endAccessor="end"
-                    style={{height: "70vh", padding:"10px 10px 10px 10px"}}
-                    onSelectEvent={handleEventSelect}
-                />
-            </div>
-            {selectedEvent && (
-                <div className="modal-overlay bg-black w-1/2">
-                    <AddInterface
-                        selectedEvent={selectedEvent}
-                        onSave={handleSave}
-                        onClose={handleClose}
-                        fromCalendar="individual"
-                    />
-                </div>
-            )}
+  const handleEventSelect = (event) => {
+    event.start = moment(event.start);
+    event.end = moment(event.end);
+    setSelectedEvent(event);
+  };
+
+  const handleSave = (event) => {
+    console.log('Event saved:', event);
+  };
+
+  const handleDelete = () => {
+    if (selectedEvent) {
+      setEvents((prevEvents) =>
+        prevEvents.filter((event) => event !== selectedEvent)
+      );
+    }
+    setSelectedEvent(null);
+  };
+
+  const handleClose = () => {
+    setSelectedEvent(null);
+  };
+
+  return (
+    <div className="calendarApp" style={{ backgroundColor: 'lightgray' }}>
+      <br />
+      <div>
+        <Calendar
+          localizer={localizer}
+          defaultDate={new Date()}
+          defaultView="week"
+          events={events}
+          startAccessor="start"
+          endAccessor="end"
+          style={{ height: '70vh', padding: '10px 10px 10px 10px' }}
+          onSelectEvent={handleEventSelect}
+        />
+      </div>
+      {selectedEvent && (
+        <div className="modal-overlay bg-black w-1/2">
+          <AddInterface
+            selectedEvent={selectedEvent}
+            onSave={handleSave}
+            onClose={handleClose}
+            onDelete={handleDelete}
+            fromCalendar="individual"
+          />
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default IndividualCalendar;
