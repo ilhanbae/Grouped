e4b1b14bf0114c24b942c1dd7c458461ed7605d8
Neil Bae <nbae0127@gmail.com>
2023-11-08 21:18:17 -0500

    Add event manager form validation
    title: required, max(32)
    start: required
    end: required, min(start)
    location: max(50)
    description: max(500)

--- front_end/src/components/EventManager/AddInterface.jsx
+++ front_end/src/components/EventManager/AddInterface.jsx
@@ -1,145 +1,211 @@
 import React, { useState, useEffect } from 'react';
 import moment from 'moment';
 import { MapPinIcon } from '@heroicons/react/24/outline';
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 
-const AddInterface = ({ onClose, onDelete, onSave, selectedEvent, fromCalendar }) => {
-  const [title, setTitle] = useState('');
-  const [start, setStart] = useState('');
-  const [end, setEnd] = useState('');
-  const [location, setLocation] = useState('');
-  const [description, setDescription] = useState('');
-  const [isSelfSelected, setIsSelfSelected] = useState(fromCalendar === 'individual');
-  const [isGroupSelected, setIsGroupSelected] = useState(!isSelfSelected);
+const AddInterface = ({
+  onClose,
+  onDelete,
+  onSave,
+  selectedEvent,
+  fromCalendar,
+}) => {
+  // Yup Schema
+  const addInterfaceSchema = yup.object().shape({
+    title: yup
+      .string()
+      .max(32, "Title too long.")
+      .required("Title is required."),
+    type: yup.string().required("Type is required."),
+    start: yup.date().required("Start Time is required."),
+    end: yup
+      .date()
+      .min(yup.ref("start"), "End time must be later than the start time.")
+      .required("End Time is required."),
+    location: yup.string().min(0).max(50, "Location too long."),
+    description: yup.string().min(0).max(500, "Description too long."),
+  });
 
-  const haveDelete = selectedEvent.title == null? false : true;
+  // useForm hook
+  const {
+    register,
+    handleSubmit,
+    watch,
+    setValue,
+    getValues,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(addInterfaceSchema),
+    defaultValues: {
+      title: selectedEvent?.title,
+      start: moment(selectedEvent.start).format("YYYY-MM-DDTHH:mm"),
+      end: moment(selectedEvent.end).format("YYYY-MM-DDTHH:mm"),
+      description: selectedEvent.descrip,
+      location: selectedEvent.location,
+      type: fromCalendar === "individual" ? "self" : "group",
+    },
+    mode: "onSubmit",
+    reValidateMode: "onSubmit",
+  });
 
+  // This hook allows user to select other time slots without closing the modal.
+  // This might not be something that we wanted, should we prevent interactions
+  // with other components when the modal is open?
   useEffect(() => {
     if (selectedEvent) {
-      setTitle(selectedEvent.title);
-      setStart(moment(selectedEvent.start).format("YYYY-MM-DDTHH:mm"));
-      setEnd(moment(selectedEvent.end).format("YYYY-MM-DDTHH:mm"));
-      setDescription(selectedEvent.descrip);
-      setLocation(selectedEvent.location);
-
-      if (fromCalendar === 'individual') {
-        setIsSelfSelected(true);
-        setIsGroupSelected(false);
-      }
+      setValue("title", selectedEvent.title);
+      setValue("start", moment(selectedEvent.start).format("YYYY-MM-DDTHH:mm"));
+      setValue("end", moment(selectedEvent.end).format("YYYY-MM-DDTHH:mm"));
+      setValue("description", selectedEvent.descrip);
+      setValue("location", selectedEvent.location);
     }
-  }, [selectedEvent, fromCalendar]);
+  }, [selectedEvent]);
 
-  const handleSave = () => {
+  // Conditional render variables
+  const haveDelete =
+    getValues("title") != null ? getValues("title") !== "" : false;
+  const isSelfSelected = watch("type") === "self";
+  const isGroupSelected = watch("type") === "group";
+
+  const toggleEventType = (type) => {
+    setValue("type", type);
+  };
+
+  // Triggered by submit button inside the form
+  // Will only trigger if the validation passes yup schema
+  const onSubmit = async (data) => {
     const event = {
-      title,
-      start,
-      end,
-      location,
-      description,
+      title: data.title,
+      start: moment(data.start).format("YYYY-MM-DDTHH:mm"),
+      end: moment(data.end).format("YYYY-MM-DDTHH:mm"),
+      location: data.location,
+      description: data.description,
     };
-
+    // console.log(event);
     onSave(event);
     onClose();
   };
 
-  const toggleSelf = () => {
-    setIsSelfSelected(true);
-    setIsGroupSelected(false);
-  };
-
-  const toggleGroup = () => {
-    setIsSelfSelected(false);
-    setIsGroupSelected(true);
-  };
-
   return (
     <div className="add-interface bg-[#e5e7eb] p-1">
-      <div className="overlay-content bg-[#e5e7eb] flex flex-col">
-        <input
-          className="h-10 m-2 text-2xl bg-white"
-          type="text"
-          placeholder="Event Title"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
+      <form
+        className="overlay-content bg-[#e5e7eb] flex flex-col"
+        autoComplete="off"
+        noValidate
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        {/* Title */}
+        <div>
+          <input
+            className="px-1 h-10 m-2 text-2xl bg-white"
+            type="text"
+            placeholder="Event Title"
+            {...register("title")}
+          />
+          <span className="block text-red-700">{errors.title?.message}</span>
+        </div>
+        {/* Type */}
         <div className="flex">
           <button
             id="Self"
             className={`h-12 px-6 m-2 text-lg rounded-lg focus:shadow-outline ${
-              isSelfSelected ? 'bg-cyan-200 text-black' : 'text-black'
+              isSelfSelected ? "bg-cyan-200 text-black" : "text-black"
             }`}
-            onClick={toggleSelf}
+            type="button"
+            onClick={() => toggleEventType("self")}
           >
             Self
           </button>
           <button
             id="Group"
             className={`h-12 px-6 m-2 text-lg rounded-lg focus:shadow-outline ${
-              isGroupSelected ? 'bg-cyan-200 text-black' : 'text-black'
+              isGroupSelected ? "bg-cyan-200 text-black" : "text-black"
             }`}
-            onClick={toggleGroup}
+            type="button"
+            onClick={() => toggleEventType("group")}
           >
             Group
           </button>
+          <span className="block text-red-700">{errors.type?.message}</span>
         </div>
-        <span className="text-lg py-2"><span>Start Time: </span>
+        {/* Start Time */}
+        <div className="flex flex-col space-y-2">
+          <span className="text-lg">
+            <span>Start Time: </span>
             <input
               className="text-lg bg-[#e5e7eb] bg-white"
               type="datetime-local"
-              value={start}
-              onChange={(e) => setStart(e.target.value)}
+              {...register("start")}
             />
-        </span>
-        <span className="text-lg py-2"><span>End Time: </span>
+          </span>
+          {/* End Time */}
+          <span className="text-lg">
+            <span>End Time: </span>
             <input
               className="text-lg bg-[#e5e7eb] bg-white"
               type="datetime-local"
-              value={end}
-              onChange={(e) => setEnd(e.target.value)}
+              {...register("end")}
             />
-        </span>
+          </span>
+          <span className="block text-red-700">{errors.end?.message}</span>
+        </div>
+        {/* Location */}
         <div className="location-container flex items-center py-2">
-        <input
-          className="text-lg bg-[#e5e7eb] bg-white"
-          type="text"
-          placeholder="Location"
-          value={location}
-          onChange={(e) => setLocation(e.target.value)}
-        />
-        <MapPinIcon className="location-icon h-6 w-6"/>
+          <input
+            className="px-1 text-lg bg-[#e5e7eb] bg-white"
+            type="text"
+            placeholder="Location"
+            {...register("location")}
+          />
+          <MapPinIcon className="location-icon h-6 w-6" />
+          <span className="block text-red-700">{errors.location?.message}</span>
         </div>
-        <textarea
-          id="description"
-          rows="4"
-          className="block py-2 w-full text-sm text-[#020617] bg-[#0ea5e9] rounded-lg border focus:ring-blue-500 focus:border-blue-500"
-          placeholder="Description"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-        ></textarea>
+        {/* Description */}
+        <div>
+          <textarea
+            id="description"
+            rows="4"
+            className="px-1 block py-2 w-full text-sm text-[#020617] bg-[#0ea5e9] rounded-lg border focus:ring-blue-500 focus:border-blue-500 resize-none"
+            placeholder="Description"
+            {...register("description")}
+          />
+          <span className="block text-red-700">
+            {errors.description?.message}
+          </span>
+        </div>
+        {/* Buttons */}
         <div className="flex justify-end mt-4">
-            <div className="flex flex-wrap">
+          <div className="flex flex-wrap">
+            {/* Confirm Button */}
+            <button
+              className="h-12 m-2 px-6 bg-gray-600 text-white"
+              type="submit"
+            >
+              Confirm
+            </button>
+            {/* Cancel Button */}
+            <button
+              className="h-12 m-2 px-6 bg-gray-600 text-white"
+              type="button"
+              onClick={onClose}
+            >
+              Cancel
+            </button>
+            {/* Delete Button */}
+            {haveDelete && (
               <button
-                onClick={handleSave}
-                className="h-12 m-2 px-6 bg-gray-600 text-white"
+                className="h-12 m-2 px-6 bg-red-400 text-white rounded-md hover:bg-red-500"
+                type="button"
+                onClick={onDelete}
               >
-                Confirm
+                Delete
               </button>
-              <button
-                className="h-12 m-2 px-6 bg-gray-600 text-white"
-                onClick={onClose}
-              >
-                Cancel
-              </button>
-              {haveDelete && (
-                  <button
-                    className="h-12 m-2 px-6 bg-red-400 text-white rounded-md hover:bg-red-500"
-                    onClick={onDelete}
-                  >
-                    Delete
-                  </button>
-                  )}
-            </div>
+            )}
+          </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 };
