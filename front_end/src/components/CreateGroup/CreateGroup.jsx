9e8925902179a0546fbb922e084d25db3a499b82
lagao <lagao@buffalo.edu>
2023-11-29 19:30:26 -0500

    Finalize components of modal

--- front_end/src/components/CreateGroup/CreateGroup.jsx
+++ front_end/src/components/CreateGroup/CreateGroup.jsx
@@ -37,27 +37,30 @@ const CreateGroup = ({ toggleSetting }) => {
   const [showSetting, setShowSetting] = useState(false);
 
   return (
-    <div className="modal-content w-auto h-96 flex flex-col justify-evenly space-y-3 bg-slate-200">
-      <div>
-          {/* Back Button */}
-          <button
-            className="float-left p-1 rounded text-white font-bold bg-slate-400 hover:bg-slate-500 display:inline"
-            type="button"
-            onClick={() => toggleSetting()}
-          >
-            Back
-          </button>
-          {/* Create Group */}
-          <button className="float-right p-1 rounded text-white font-bold bg-slate-400 hover:bg-slate-500 display:inline">
-            Create Group
-          </button>
-      </div>
-      {/* Search Field */}
-      <input
-          id="search"
-          className="p-1 w-full text-lg text-[#660033] text-center bg-white rounded border placeholder-slate-300 focus:[#ffcccc] focus:[#ebbcbc] resize-none"
-          placeholder="Search"
-        />
+    <div className="modal-content w-full h-auto flex flex-col justify-evenly space-y-3 bg-slate-200">
+      <form>
+          {/* Title Field */}
+          <input
+              id="Title"
+              className="p-1 w-full mt-5 text-lg text-[#660033] text-center bg-white rounded border placeholder-slate-300 focus:[#ffcccc] focus:[#ebbcbc] resize-none"
+              placeholder="Title"
+              type="text"
+            />
+          {/* Description Field */}
+          <textarea
+            id="Description"
+            rows="3"
+            className="p-1 w-full text-lg mt-5 text-[#660033] text-center bg-white rounded border placeholder-slate-300 focus:[#ffcccc] focus:[#ebbcbc] resize-none"
+            placeholder="Description"
+            type="text"
+            />
+          <div className="m-2">
+              {/* Cancel Button*/}
+              <button className="float-left p-1 rounded text-white font-bold bg-slate-400 hover:bg-slate-500 display:inline" type="button">Cancel</button>
+              {/* Save Button*/}
+              <button className="float-right p-1 rounded text-white font-bold bg-slate-400 hover:bg-slate-500 display:inline" type="submit">Save</button>
+          </div>
+      </form>
     </div>
   );
 };
