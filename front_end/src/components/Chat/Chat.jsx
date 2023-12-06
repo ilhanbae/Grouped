1452253c5b9970122b559453e3ccef5847f2325b
lagao <lagao@buffalo.edu>
2023-12-06 02:47:56 -0500

    Modified css, layout, and message tracking
    	- Added timestamp and id to each message
    	- If its the first message in a group chat: name, message, and
    	  timestamp will always be displayed
    	- If the message to send is from a different user than the
    	  previous message, then username will be displayed regardless
    	  of time
    	- If the last message was sent within 3 minutes, then there will
    	  be no timestamp printed
    	- Modified padding to better display messages
    	- Incorporated text wrapping for long long messages (long word
    	  and long paragraphs)
    	- Left functionality and commented to simulate message recieved
    	  and tested above points with it
    	- checked punctuation marks in word count for dynamic message
    	  length and ensured responsiveness

--- front_end/src/components/Chat/Chat.jsx
+++ front_end/src/components/Chat/Chat.jsx
@@ -6,10 +6,10 @@ const Chat = ({ onClose, goBack }) => {
   const [inputText, setInputText] = useState('');
   const [userId, setUserId] = useState('');
   const [messages, setMessages] = useState([
-    { user_id: 'Me', text: 'worst group ever', isSent: true },
-    { user_id: 'Danny', text: 'what group!?', isSent: false },
-    { user_id: 'Me', text: 'You know *******', isSent: true },
-    { user_id: 'Danny', text: 'o shoot, them???', isSent: false },
+    { id: 1, user_id: 'Me', text: 'worst group ever', isSent: true, currentTime: new Date() },
+    { id: 2, user_id: 'Danny', text: 'what group!?', isSent: false, currentTime: new Date() },
+    { id: 3, user_id: 'Me', text: 'You know *******', isSent: true, currentTime: new Date() },
+    { id: 4, user_id: 'Danny', text: 'o shoot, them???', isSent: false, currentTime: new Date() },
   ]);
   const chatContainerRef = useRef(null);
 
@@ -19,12 +19,18 @@ const Chat = ({ onClose, goBack }) => {
 
   const handleSendMessage = () => {
     if (inputText.trim() !== '') {
-      const sendMessage = { user_id: "Me", text: inputText, isSent: true };
+      const sendMessage = {
+        id: messages.length + 1,
+        user_id: "Me",
+        text: inputText,
+        isSent: true,
+        currentTime: new Date()
+      };
       setMessages([...messages, sendMessage]);
       setInputText('');
       // Simulate a received message after a short delay
 //       setTimeout(() => {
-//         setMessages([...messages, sendMessage, { user_id: userId, text: 'Received: ' + inputText, isSent: false }]);
+//         setMessages([...messages, sendMessage, { user_id: "Danny", text: 'Received: ' + inputText, isSent: false }]);
 //       }, 500);
     }
   };
@@ -46,7 +52,7 @@ const Chat = ({ onClose, goBack }) => {
     const elem = document.createElement('span');
     elem.style.visibility = 'hidden';
     elem.style.position = 'absolute';
-    elem.style.whiteSpace = 'pre';
+    elem.style.whiteSpace = 'pre-wrap';
 
     // Replace spaces with a non-breaking space to preserve multiple spaces
     elem.innerText = text.replace(/ /g, '\u00A0');
@@ -58,38 +64,92 @@ const Chat = ({ onClose, goBack }) => {
     return width;
   };
 
+
+  const formatTimestamp = (timestamp) => {
+    const options = { hour: '2-digit', minute: '2-digit'};
+    return new Intl.DateTimeFormat('en-US', options).format(timestamp);
+  };
+
+  const shouldDisplayUserInfo = (message, index) => {
+    if (index === 0) {
+      // Always display user info and timestamp for the first message
+      return { displayUserId: true, displayTimestamp: true };
+    }
+
+    const prevMessage = messages[index - 1];
+
+    // Check if the user_id should be displayed
+    const shouldDisplayUserId = message.user_id !== prevMessage.user_id;
+
+    // Check if the timestamp should be displayed (if the messages are from different senders)
+    const shouldDisplayTimestamp =
+      shouldDisplayUserId || (message.currentTime - prevMessage.currentTime) > 180000;
+
+    // If the timestamp difference is less than 3 minutes, don't display timestamp
+    const displayTimestamp =
+      shouldDisplayTimestamp && (message.currentTime - prevMessage.currentTime) > 180000;
+
+    return {
+      displayUserId: shouldDisplayUserId,
+      displayTimestamp: displayTimestamp,
+    };
+  };
+
   return (
     <div className="chat-container">
       <div className="chat-header">
         <span className="flex alignItems-center" onClick={goBack}>
-            <ArrowLeftIcon className="h-6 w-6"/>
-             PM Rulers
+          <ArrowLeftIcon className="h-6 w-6" />
+          PM Rulers
         </span>
         <button className="close-button" onClick={onClose}>
           &times;
         </button>
       </div>
       <div className="chat-messages" ref={chatContainerRef}>
-        {messages.map((message, index) => (
-          <div key={index} className={`chat-message ${message.isSent ? 'sent' : 'received'}`}
-            style={{width: `${calculateTextWidth(message.text) + 50}px`}}>
-            {message.isSent ? (
-              <div className="message-sent">
+        {messages.map((message, index) => {
+          const { displayUserId, displayTimestamp } = shouldDisplayUserInfo(message, index);
+          return (
+            <div
+              key={index}
+              className={`chat-message ${message.isSent ? 'sent' : 'received'}
+                ${(!shouldDisplayUserInfo(message, index)) ? 'compact-message' : ''}`
+              }
+            >
+              {message.isSent ? (
+                <div className="message-sent">
                   <span className="user-info">
-                    <span className="user-id sender">{message.user_id}</span>
+                    <span className="user-id sender">
+                      {displayUserId && message.user_id}
+                    </span>
                   </span>
-                  <span className="message-text-sent">{message.text}</span>
+                  <span className="message-text-sent"
+                    style={{ width: `${calculateTextWidth(message.text) + 20}px` }}>
+                    {message.text}
+                  </span>
+                  <span className="timestamp sender">
+                    {displayTimestamp && formatTimestamp(message.currentTime)}
+                  </span>
                 </div>
-            ) : (
-               <div className="message-received">
+              ) : (
+                <div className="message-received">
                   <span className="user-info">
-                    <span className="user-id">{message.user_id}</span>
+                    <span className="user-id">
+                      {displayUserId && message.user_id}
+                    </span>
                   </span>
-                  <span className="message-text-received">{message.text}</span>
+                  <span className="message-text-received"
+                    style={{ width: `${calculateTextWidth(message.text) + 20}px` }}>
+                    {message.text}
+                  </span>
+                  <span className="timestamp">
+                    {displayTimestamp && formatTimestamp(message.currentTime)}
+                  </span>
                 </div>
-            )}
-          </div>
-        ))}
+              )}
+            </div>
+          );
+        })}
       </div>
       <div className="chat-input">
         <input
